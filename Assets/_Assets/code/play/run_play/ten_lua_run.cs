cef25451ddaaf7ffa3909c5de38d3ba7b4e0e67f
phamviet1121 <pahmvit4454@gmail.com>
2024-09-24 21:16:37 +0700

    tao_tenlua

--- Assets/_Assets/code/play/run_play/ten_lua_run.cs
+++ Assets/_Assets/code/play/run_play/ten_lua_run.cs
@@ -1,28 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
+
 
 public class ten_lua_run : MonoBehaviour
 {
-    public Transform tenlua; 
-    public float rotationSpeed = 5f; 
+    public GameObject tenlua;
+    public float rotationSpeed = 1f;
+    private GameObject newtenlua;
+    float tenlualnewtime = 5f;
+    float m_tenluanewtime;
+    public GameObject play;
+    private void Start()
+    {
+        if (newtenlua != null)
+        {
+            Destroy(newtenlua);
+        }
 
+
+    }
     void Update()
     {
-        if (tenlua != null)
+        float a = Random.Range(Random.Range(10, 20), Random.Range(-10, -20));
+        float b = Random.Range(Random.Range(15, 20), Random.Range(-15, -20));
+        Vector2 vitri = new Vector2(transform.position.x + a, transform.position.y + b);
+        if (play.activeSelf == true && newtenlua == null)
         {
-           
-            Vector3 directionToPlayer =   transform.position-tenlua.position;
-
-           
-            float angle = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg;
-
-           
-            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            m_tenluanewtime -= Time.deltaTime;
 
 
-            tenlua.transform.rotation = Quaternion.Slerp(tenlua.transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+
+            if (newtenlua == null && m_tenluanewtime <= 0)
+            {
+
+                newtenlua = Instantiate(tenlua, vitri, Quaternion.identity);
+
+                m_tenluanewtime = tenlualnewtime;
+            }
+        }
+        if (newtenlua != null)
+        {
+            runtenlua(newtenlua);
+        }
+
+
+
+    }
+    public void runtenlua(GameObject newruntenlua)
+    {
+
+
+        Vector3 directionToPlayer = transform.position - newruntenlua.transform.position;
+
+
+        float angle = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg;
+
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+
+        newruntenlua.transform.rotation = Quaternion.Slerp(newruntenlua.transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        Debug.Log($"toa do ten lua{tenlua.transform.rotation}");
+
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("tenlua"))
+        {
+            Destroy(newtenlua);
         }
     }
 }
