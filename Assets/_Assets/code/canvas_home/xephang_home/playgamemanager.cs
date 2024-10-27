fbbf063ae7673408fa9c0213044e1c7a911511b8
phamviet11211 <phamviet4454@gmail.com>
2024-10-27 15:36:03 +0700

    google play

--- Assets/_Assets/code/canvas_home/xephang_home/playgamemanager.cs
+++ Assets/_Assets/code/canvas_home/xephang_home/playgamemanager.cs
@@ -1,12 +1,15 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
+using UnityEngine.UI;
 public class playgamemanager : MonoBehaviour
 {
     public TextMeshProUGUI Details_txt;
+    public Button leaderboardButton; // Thêm biến để lưu trữ nút xếp hạng
+    private bool isSignedIn = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,19 +18,22 @@ public class playgamemanager : MonoBehaviour
     public void SignIn()
     {
         PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+        leaderboardButton.onClick.AddListener(OpenLeaderboard); // Gắn sự kiện cho nút
     }
     internal void ProcessAuthentication(SignInStatus status)
     {
         if (status == SignInStatus.Success)
         {
+            isSignedIn = true;
             // Continue with Play Games Services
             string name = PlayGamesPlatform.Instance.GetUserDisplayName();
-            string id= PlayGamesPlatform.Instance.GetUserId();
-            string ImgUrl= PlayGamesPlatform.Instance.GetUserImageUrl();
-            Details_txt.text = "Success\n"+name;
+            string id = PlayGamesPlatform.Instance.GetUserId();
+            string ImgUrl = PlayGamesPlatform.Instance.GetUserImageUrl();
+            Details_txt.text = "Success\n" + name;
         }
         else
         {
+            isSignedIn = false;
             Details_txt.text = "Sign in Failed!";
             // Disable your integration with Play Games Services or show a login button
             // to ask users to sign-in. Clicking it should call
@@ -39,5 +45,23 @@ public class playgamemanager : MonoBehaviour
     void Update()
     {
 
+    }
+    public void OpenLeaderboard()
+    {
+        if (isSignedIn)
+        {
+            // Mở trang xếp hạng nếu đã đăng nhập
+            PlayGamesPlatform.Instance.ShowLeaderboardUI();
+        }
+        else
+        {
+            Details_txt.text = "Bạn cần đăng nhập trước!";
+        }
+
+
+    }
+    public bool IsUserSignedIn()
+    {
+        return isSignedIn;
     }
 }
